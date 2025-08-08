393a230bdc6a5a84dd5b658a4f89dbb83cfb846c
solace <onyedikajah.josiah07@gmail.com>
2025-08-08 10:21:39 +0100

    class for java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        add();
+    }
+
+    public static int add(){
+        int c = 2 + 10;
+        System.out.println(c);
+        return c;
+    }
+
+}
+
+//static
+//main file [o]
+//return type = int, string, bool, double
+
+//javac Main.java
+//java Main
